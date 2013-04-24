8aa19382625adfec9c817617a78bca163e239626
Phil <pcao@risd.edu>
2013-04-24 02:44:37 -0400

    Revert "Changed BG back"
    This reverts commit a47c5bcda4428f16b5e1ba69aa6d6af7e7b987c7.

--- index.php
+++ index.php
@@ -3,34 +3,11 @@
 
 <div class="heroContainer">
 	<div class="heroText">
-		<h1>Second Life is a student-run, non-profit materials upcyling center located in Providence, Rhode Island. 
+	<h1>Second Life is a student-run, non-profit materials upcyling center located in Providence, Rhode Island. 
 	</div>
 </div>
 
 <div class="aboutContainer">
-	<div class="aboutModule" id="hours">
-		<h1>Hours</h1>
-
-			<ul>
-				<li> <em>M - T</em> 3:00 - 4:00</li>
-				<li> <em>M - T</em> 3:00 - 4:00</li>
-			</ul>
-
-	</div>
-
-
-	<div class="aboutModule" id="contact">
-		<h1>Contact</h1>
-
-			<ul>
-				<li><a href="mailto:secondlife@risd.edu">secondlife@risd.edu</a></li>
-				<li>401-401-401</li>
-			</ul>
-
-	</div>
-
-	<div class="aboutText">
-	</div>
 </div>
 
 
